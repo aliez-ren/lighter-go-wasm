5a1057cf7c7dbe2d5303bbc3784117c9675cfbee
Aliez <8287771+aliez-ren@users.noreply.github.com>
2026-01-08 08:03:35 +0900

    flake

---
+++ flake.nix
@@ -0,0 +1,29 @@
+{
+  description = "Dev environment";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  };
+
+  outputs =
+    { self, nixpkgs }:
+    let
+      supportedSystems = [
+        "x86_64-linux"
+        "aarch64-darwin"
+      ];
+
+      forAllSystems = nixpkgs.lib.genAttrs supportedSystems;
+
+      nixpkgsFor = forAllSystems (system: import nixpkgs { inherit system; });
+    in
+    {
+      devShells = forAllSystems (system: {
+        default = nixpkgsFor.${system}.mkShell {
+          packages = with nixpkgsFor.${system}; [
+            go
+          ];
+        };
+      });
+    };
+}
